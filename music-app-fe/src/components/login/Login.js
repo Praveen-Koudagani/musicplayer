fe48ca4a89d166fe8f3f78106cba984891006364
Aashritha Donapati <Aashritha_Donapati@epam.com>
2022-06-02 13:42:10 +0530

    "commit"

--- music-app-fe/src/components/login/Login.js
+++ music-app-fe/src/components/login/Login.js
@@ -1,10 +1,38 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import Register from '../register/Register';
+import React, { useState } from 'react';
+import { Link, useNavigate} from 'react-router-dom';
+import axios from "axios";
+import $ from "jquery";
+
 
 const Login = () => {
-const handleLogin=(event)=>{
-  console.log("cliked on login");
+  const navigate=useNavigate();
+  const [user, setUser] = useState({
+    username: "",
+    password: "", 
+  })
+  const handleInputs = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+    setUser({...user, [name]: value})
+  }
+
+  async function handleLogin(event) {
+    event.preventDefault();
+    const response = await axios.post(`http://localhost:8080/api/auth/login`,{
+      'username': user.username,
+      'password': user.password
+    }).then(console.log("hi"));
+    const clean = () => {
+        $('input[name="username"]').value="";
+        $('input[name="password"]').value="";
+    }
+    clean();
+    console.log(response);
+    navigate("/Home");
+  }
+
+const handleLogin2=(event)=>{
+  console.log("cliked on login"+user.username+":"+user.password);
 }
   return (
     <div>
@@ -13,9 +41,9 @@ const handleLogin=(event)=>{
         <div className=''>
         <label>Login</label>
         <br/>
-        <input type="email" name="email" placeholder='enter email'></input>
+        <input type="text" name="username" placeholder='enter email'onChange={handleInputs}></input>
         <br/>
-        <input type="password" name="userpassword" placeholder='enter password'></input>
+        <input type="password" name="password" placeholder='enter password'onChange={handleInputs}></input>
         <input type="submit" name="login" value="Login" ></input>
         </div>
         <div className="container Login">
